4c6ed2040e7befa97573a68a217aeb02084e77a1
Manas Mishra <manasmishra2520@gmail.com>
2022-03-07 19:02:26 +0530

    initial app completed

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,11 +1,10 @@
-import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:clima/utilities/constants.dart';
+import 'package:location/location.dart';
 
 Future getLocationWeather() async {
-  Location location = Location();
-  await location.getLocation();
-
+  var currentLocation = Location();
+  var location = await currentLocation.getLocation();
   NetworkHelper networkHelper = NetworkHelper(
       'http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$kApiKey&units=metric');
   var weatherData = await networkHelper.getData();
